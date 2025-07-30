1215c09e10eb0e10444ff6bd7fada9258eeb5e5a
sumit kumar <sumitsaini95488@gmail.com>
2025-07-30 16:12:46 +0530

    feat(ui): try all operaters

---
+++ PS2/problem1.py
@@ -0,0 +1,10 @@
+# Wite a python program to add two numbers and try all operaters
+a = int(input("Enter value 1: "))
+b = int(input("Enter value 2: "))
+
+print("Sum is:-", a + b)
+print("diff is:-", a - b)
+print("multiple is:-", a * b)
+print("division is:-", a / b)
+print("reminder is:-", a % b)
+print("square is:-", a ** b)
